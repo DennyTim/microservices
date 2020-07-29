41ef895bfb7d9a64c290bbecafc89aefffac6340
dety0819 <luckystudydanit@gmail.com>
2020-07-30 01:32:05 +0300

    Testing for auth

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,7 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
+import { app } from './app';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from './middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(cookieSession({
-	signed: false,
-	secure: true
-}));
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-app.all('*', async (req, res) => {
-	throw new NotFoundError();
-});
-
-app.use(errorHandler);
-
-const start = async () => {
+(async () => {
 	if (!process.env.JWT_KEY) {
 		throw new Error('JWT_KEY must be defined')
 	}
@@ -49,6 +20,4 @@ const start = async () => {
 	app.listen(3000, () => {
 		console.log('Listening on port 3000!!!!!!!!');
 	});
-};
-
-start();
+})();
